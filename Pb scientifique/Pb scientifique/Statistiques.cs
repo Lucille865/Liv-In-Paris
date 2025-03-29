175baf8880170d763b8730c1708f20c9c07eccfe
Lucille865 <lucille.bon007@gmail.com>
2025-03-29 11:27:52 +0100

    Add files via upload

---
+++ Pb scientifique/Pb scientifique/Statistiques.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pb_scientifique
+{
+    public class Statistiques
+    {
+        public static void AfficherStatistiques(List<Commande> commandes)
+        {
+            Console.WriteLine("Statistiques des commandes...");
+        }
+    }
+}
---
+++ Pb scientifique/Pb scientifique/Utilisateur.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pb_scientifique
+{
+    public abstract class Utilisateur
+    {
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public string Adresse { get; set; }
+        public string Telephone { get; set; }
+        public string Email { get; set; }
+        public string Identifiant { get; set; }
+        public string MotDePasse { get; set; }
+    }
+}
